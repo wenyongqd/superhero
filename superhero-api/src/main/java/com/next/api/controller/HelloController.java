c017011407304f92eb332744b419449ca3adc623
wenyongqd <wenyongqd@gmail.com>
2020-06-23 23:51:13 -0400

    update new project

---
+++ superhero-api/src/main/java/com/next/api/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.next.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object hello() {
+        return "hello world";
+    }
+}
